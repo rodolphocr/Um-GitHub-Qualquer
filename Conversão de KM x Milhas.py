37782542be5bc684e294dba5ad07dc79624dbd74
rodolphocr <rcricci01@gmail.com>
2022-08-13 00:19:59 -0300

    ''

---
+++ Conversão de KM x Milhas.py
@@ -0,0 +1,6 @@
+#km =float(input('Entre com os KM: '))
+#milhas=float(1.609)
+#result = km / milhas
+#print('KM convertido em milhas são: ',result)
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+# dia=input('Dia =')
+# mes=input('Mês =')
+# ano=input('Ano =')
+# print('Você nasceu no dia', dia, 'de', mes, 'de', ano,'. Correto?')
+
+# primeiro=int(input('Entre com o primeiro número: '))
+# segundo=int(input('Entre com o segundo número: '))
+# soma=(primeiro) + (segundo)
+# print('Resultado: ', soma)
