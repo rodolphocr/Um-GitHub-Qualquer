6d37ee4ed2cfa961ad38a6ff5f9b2fc360ecd146
rodolphocr <111164026+rodolphocr@users.noreply.github.com>
2022-08-13 00:06:41 -0300

    Add files via upload

---
+++ Curso Python #06.py
@@ -0,0 +1,55 @@
+# Tipos Primitivos e Saída de Dados
+
+# SOMA DE 2 NÚMEROS(inteiros_"int") E MOSTRAR O RESULTADO.
+# n1 = int(input('Digite um número: '))
+# n2 = int(input('Digite um número: '))
+# resultado = n1 + n2
+# print('Resultado da soma é : ', resultado)
+
+# SOMA DE 2 NÚMEROS(reais_"float") E MOSTRAR O RESULTADO.
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# resultado = n1 + n2
+# print('Resultado da soma é : ', resultado)
+
+# SOMA DE 2 NÚMEROS + MENSAGEM DE RESPOSTA + SAÍDA DE RESULTADO
+# n1 = int(input('Digite um número: '))
+# n2 = int(input('Digite um número: '))
+# resultado = n1 + n2
+# print('A SOME ENTRE',  n1,  'e ',  n2, 'VALE ', ':', resultado)
+# print("A Soma entre {0} e {1} vale {2}".format(n1, n2, resultado))
+
+# MOSTAR O TIPO DA VARIAVEL
+# n=input('Digite um valor: ')
+# print(type(n))
+
+# DESCOBRIR QTOS ANOS A PESSOA TEM.
+# ano_nascimento = int(input('Entre com o ano que você nasceu: '))
+# ano = (2022)
+# idade = ano - ano_nascimento
+# print('Você tem {} '.format(idade))
+
+# CHECAGEM DE VELOCIDADE.
+# v=int(input('Entre com a velocidade da pista: '))
+# if v > 110:
+   # print('Acima da velocidade permitida')
+   # print('Reduza a velocidade')
+# elif  v < 60:
+   # print('Aumente a velocidade até 80KM/h')
+#else:
+    # print('Velocidade ok')
+
+#CONVERTER KM/H EM MILHAS
+#km =float(input('Entre com os KM: '))
+#milhas=float(1.609)
+#result = km / milhas
+#print('KM convertido em milhas são: ',result)
+
+#CHECAR O FINANCIMANETO.
+#renda=float(input('Entre com a reda: '))
+#nome=str(input('Nome limpo sim ou não: '))
+#if renda >= 5000 and nome == "sim":
+    #print('Financiamento aprovado')
+#else:
+    #print('Financiamento não aprovado')
+
