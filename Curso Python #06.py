d4bd4d4ece6b7ed2f3c2e45b1b282f44c1e455e5
rodolphocr <rcricci01@gmail.com>
2022-08-13 01:14:26 -0300

    ''

--- Curso Python #06.py
+++ Curso Python #06.py
@@ -39,3 +39,12 @@
 #else:
     #print('Financiamento não aprovado')
 
+# dia=input('Dia =')
+# mes=input('Mês =')
+# ano=input('Ano =')
+# print('Você nasceu no dia', dia, 'de', mes, 'de', ano,'. Correto?')
+
+# primeiro=int(input('Entre com o primeiro número: '))
+# segundo=int(input('Entre com o segundo número: '))
+# soma=(primeiro) + (segundo)
+# print('Resultado: ', soma)
