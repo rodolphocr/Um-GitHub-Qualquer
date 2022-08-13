9498067b531576eac8443ec33d9317cb916a5f15
rodolphocr <rcricci01@gmail.com>
2022-08-13 00:39:08 -0300

    ''

--- Conversão de KM x Milhas.py
+++ Conversão de KM x Milhas.py
@@ -1,6 +1,4 @@
-#km =float(input('Entre com os KM: '))
-#milhas=float(1.609)
-#result = km / milhas
-#print('KM convertido em milhas são: ',result)
-
-
+# km =float(input('Entre com os KM: '))
+# milhas=float(1.609)
+# result = km / milhas
+# print('KM convertido em milhas são: ',result)
