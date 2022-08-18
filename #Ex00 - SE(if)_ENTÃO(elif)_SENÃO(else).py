f0ab36acf6ac80f5599a95758309d3c3b0d04a3a
rodolphocr <rcricci01@gmail.com>
2022-08-18 01:54:24 -0300

    ''

--- #Ex00 - SE(if)_ENTÃO(elif)_SENÃO(else).py
+++ #Ex00 - SE(if)_ENTÃO(elif)_SENÃO(else).py
@@ -36,3 +36,59 @@ if m >=6:
     print('Sua média foi {} então você passou.'.format(m))
 else:
     print('Sua média foi {} então você não passou.'.format(m))
+
+
+#FINANCIMENTO DE VEÍCULO COM DESCONTO.
+automovel = str(input('Carro ou Moto: '))
+while automovel == 'carro':
+    print('Entre com os dados dos valores abaixo.')
+    break
+while automovel == 'moto':
+    print('Não temos mais motos no estoque.')
+    exit()
+preco = float(input('Digite valor do carro: '))
+if preco > 50000:
+    porcentagem = (preco * 10  / 100)
+    novo = (preco - porcentagem)
+    print('Voce tem R$ {:.0f} de desconto.'.format(preco * 10 / 100))
+    print('Sendo o valor do carro de R${:.0f}'.format(novo))
+else:
+    print("Valores de automoveis menores de R$50.000,00 não tem desconto.")
+
+# Faça um Programa que peça dois números e imprima o maior deles.
+n1 = int(input('Entre com o primeiro número: '))
+n2 = int(input('Entre com o segundo número: '))
+
+if n1 > n2:
+    print('O maior número informado é {}'.format(n1))
+else:
+    print('O maior número informado é {}'.format(n2))
+
+# Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+# Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+letra = str(input('Entre com a letra F para Feminino or M para Masculino: '))
+if letra == 'F':
+    print('FEMININO')
+if letra == 'M':
+    print('MASCULINO')
+elif letra != 'F' and 'M':
+    print('Sexo Inválido.')
+
+# Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+# A mensagem "Reprovado", se a média for menor do que sete;
+# A mensagem "Aprovado com Distinção", se a média for igual a dez.
+# Comando exit(), é uma função que termina um outro comando ou finaliza um programa ou função.
+materia = str(input('Entre com a matéria de referencia: '))
+nota1 = float(input('Entre com a primeira nota: '))
+nota2 = float(input('Entre com a segunda nota: '))
+soma = (nota1 + nota2)
+media = (soma / 2)
+print('Sua média é {}.'.format(media))
+while media == 10:
+    print('Aprovado com distinção na máteria de {}.'.format(materia))
+    exit()
+if media < 7:
+    print('Reprovado na matéria de {}, estude mais.'.format(materia))
+elif media >= 7:
+    print('Você foi aprovado na máteria de {}.'.format(materia))
--- Curso Python #06.py
+++ Curso Python #06.py
@@ -19,3 +19,41 @@
 # segundo=int(input('Entre com o segundo número: '))
 # soma=(primeiro) + (segundo)
 # print('Resultado: ', soma)
+
+#Faça um Programa que peça dois números e imprima o maior deles.
+n1 = int(input('Entre com o primeiro número: '))
+n2 = int(input('Entre com o segundo número: '))
+
+if n1 > n2:
+    print('O maior número informado é {}'.format(n1))
+else:
+    print('O maior número informado é {}'.format(n2))
+
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+# Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+letra = str(input('Entre com a letra F para Feminino or M para Masculino: '))
+if letra == 'F':
+    print('FEMININO')
+if letra == 'M':
+    print('MASCULINO')
+elif letra != 'F' and 'M':
+    print('Sexo Inválido.')
+
+# Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+# A mensagem "Reprovado", se a média for menor do que sete;
+# A mensagem "Aprovado com Distinção", se a média for igual a dez.
+# Comando exit(), é uma função que termina um outro comando ou finaliza um programa ou função.
+materia = str(input('Entre com a matéria de referencia: '))
+nota1 = float(input('Entre com a primeira nota: '))
+nota2 = float(input('Entre com a segunda nota: '))
+soma = (nota1 + nota2)
+media = (soma / 2)
+print('Sua média é {}.'.format(media))
+while media == 10:
+    print('Aprovado com distinção na máteria de {}.'.format(materia))
+    exit()
+if media < 7:
+    print('Reprovado na matéria de {}, estude mais.'.format(materia))
+elif media >= 7:
+    print('Você foi aprovado na máteria de {}.'.format(materia))
