eb0a8eae174346f4b3234f5329472c0bd2e84996
rodolphocr <rcricci01@gmail.com>
2022-08-28 16:50:00 -0300

    ''

--- Curso Python #06.py
+++ Curso Python #06.py
@@ -1,1 +1,11 @@
-
+# Formula para descobrir idadde:
+from goto_py import  goto
+nu_calcado = str(input('Entre o numero do seu sapato: '))
+zero = str('00')
+resultado = int(nu_calcado + zero)
+ano_nascimento = int(input('Enter com o ano do nascimento: '))
+resulado1 = int(resultado - ano_nascimento)
+ano_atual = int(input('Entre como ano atual: '))
+idade = int(resulado1 + ano_atual)
+print('Os ultimos 2 digitos sao a sua idade {}'.format(idade))
+goto(3)
