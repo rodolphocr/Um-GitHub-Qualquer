2ad112d20319019a28f903cdef6d56993d0d5fa9
rcricci01 <rcricci01@gmail.com>
2022-08-31 01:32:33 -0300

    ''v

--- ex006.py
+++
@@ -1,8 +0,0 @@
-#CALCULAR DOBRO, O TRIPO E A RAIZ QUADRA DE UM NÚMERO.
-num = int(input('Entre com um número: '))
-r1 = (num * 2)
-print(r1)
-r2 = (num * 3)
-print(r2)
-r3 = (num **(1/2))
-print(r3)
--- ex007.py
+++
@@ -1,6 +0,0 @@
-#CALCULAR A MÉDIA DE 2 NOTAS.
-nota1 = float(input('Entre com a nota 1: '))
-nota2 = float(input('Entre com a nota 2: '))
-result = (nota1 + nota2)
-media = (result / 2)
-print('A media da nota é {}.'.format(media))
