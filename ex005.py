05cecbca0a2a8ed1cdc46c330962ab5ab85a6219
Rodolpho C. Ricci <111164026+rodolphocr@users.noreply.github.com>
2022-08-31 01:16:22 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,5 @@
+#SUCESSOR E ANTECESSOR DE UM NÚMERO.
+num = float(input('Entre com um valor para saber seu sucessor e antecessor: '))
+r1 = (num + 1)
+r2 = (num - 1)
+print('O número sucessor de {} é {} e o antecessor de {}'.format(num, r1, r2))
---
+++ ex007.py
@@ -0,0 +1,6 @@
+#CALCULAR A MÉDIA DE 2 NOTAS.
+nota1 = float(input('Entre com a nota 1: '))
+nota2 = float(input('Entre com a nota 2: '))
+result = (nota1 + nota2)
+media = (result / 2)
+print('A media da nota é {}.'.format(media))
